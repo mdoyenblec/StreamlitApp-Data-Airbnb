ae05c91d348686579fb4d66c3540bd8d64e68e1e
Maxime Doyen Blec <89549493+mdoyenblec@users.noreply.github.com>
2023-12-22 16:10:40 +0100

    Create Amsterdam_airbnb_data.py

---
+++ pages/Amsterdam_airbnb_data.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.title('Airbnb Amsterdam Analyse de marché V2')
+st.title('Hello')
+
+DATA_URL = ('http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2023-12-12/visualisations/listings.csv')
+
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    return data
+
+data_load_state = st.text('Chargement des données...')
+data = load_data(10000)
+data_load_state.text("Fait! (using st.cache)")
+
+if st.checkbox('Afficher les données raw'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Prix Moyen à Amsterdam')
+average_price_amsterdam = data['price'].mean()
+st.write(f"Le prix moyen à Amsterdam est de {average_price_amsterdam:.2f} €")
+
+st.subheader('Prix Moyen par Quartier (Interactif)')
+average_price_per_neighborhood = data.groupby('neighbourhood')['price'].mean().reset_index()
+fig = px.bar(average_price_per_neighborhood, x='neighbourhood', y='price', 
+             labels={'price': 'Prix Moyen', 'neighbourhood': 'Quartier'},
+             title="Prix Moyen par Quartier à Amsterdam")
+st.plotly_chart(fig)
+
+st.subheader('Nombre de Biens par Quartier')
+count_per_neighbourhood = data['neighbourhood'].value_counts()
+st.bar_chart(count_per_neighbourhood)
+
+st.subheader('Carte Dynamique avec Prix des Logements')
+st.map(data)
+
+st.subheader('Nombre de Logements par Propriétaire')
+properties_per_owner = data['host_name'].value_counts().head(10)  
+st.bar_chart(properties_per_owner)
+
+
+
+        
